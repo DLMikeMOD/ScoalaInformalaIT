a3ae58176f794af30df8a1146ec3b4b1fd96704b
DLMikeMOD <fake_mail_mike@yahoo.com>
2022-01-28 08:35:14 +0200

    Hangman 2.0 Fixed and improved

--- 03-data-structures/hagman.py
+++ 03-data-structures/hagman.py
@@ -16,7 +16,7 @@ while numar_incercari <= 7:
         if litera.lower() not in (lista_litere_incercate):
             numar_incercari += 1
         lista_litere_incercate.add(litera.lower())
-        print (f'Litenra nu exista, deja ai incercat urmatorele litere {",".join(lista_litere_incercate)}')
+        print (f'Litera nu exista, deja ai incercat urmatorele litere {",".join(lista_litere_incercate)}')
         print (f"Mai ai {8 - numar_incercari} incercari")
 
     if numar_incercari > 7:
