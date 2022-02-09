5e4cbe5b468adea30925ee12010854a450494d94
DLMikeMOD <fake_mail_mike@yahoo.com>
2022-02-09 18:14:32 +0200

    Curs 05

---
+++ Teme/CNP validator.py
@@ -0,0 +1,41 @@
+if __name__ == '__main__':
+    cnp = input("Introdu CNP-ul: ")
+
+    if len(cnp) != 13:
+        for i in cnp:
+            if '0' > i < '9':
+                print('contains not digits')
+                break
+        print('invalid')
+    else:
+        sex = cnp[0]
+        an = cnp[1:3]
+        luna = cnp[3:5]
+        zi = cnp[5:7]
+        jj = cnp[7:9]
+        nnn = cnp[9:12]
+        c = cnp[-1]
+
+        sex_ok = '1' <= sex <= '9'
+        an_ok = '00' <= an <= '99'
+        luna_ok = '01' <= luna <= '12'
+        zi_ok = '01' <= zi <= '31'
+        jj_ok = '01' <= jj <= '52'
+        nnn_ok = '001' <= nnn <= '999'
+
+        c_control = 0
+        nr_control = '279146358279'
+        for index, i in enumerate(cnp[:12]):
+            c_control += int(i) * int(nr_control[index])
+
+        c_control = c_control % 11
+
+        if c_control == 10:
+            c_control = 1
+
+        c_ok = c_control == int(c)
+
+        if sex_ok and an_ok and luna_ok and zi_ok and jj_ok and nnn_ok and c_ok:
+            print("CNP Valid, esti safe")
+        else:
+            print ("CNP INVALID")
