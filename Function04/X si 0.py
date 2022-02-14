cd849b87c3bcab1a8a8aa5b24dece266d739e87e
DLMikeMOD <fake_mail_mike@yahoo.com>
2022-02-14 21:22:09 +0200

    Curs 06 Args, kargs, map si zip

--- Function04/X si 0.py
+++ Function04/X si 0.py
@@ -7,7 +7,10 @@ pierde = 0
 X = 'X'
 O = 'O'
 
+
 # Condidii peste conditii
+
+
 def conditii_castig():
     global a1, a2, a3, a4, a5, a6, a7, a8, a9
     global o_castiga, x_castiga, remiza
@@ -51,40 +54,45 @@ def conditii_castig():
     if o_castiga:
         castiga = True
         return print('Castigatorul este', nume_jucator.upper(), 'BRAVOS' '\n--Runda Terminata--\n'), castiga
-    if a1 != '-' and a2 != '-' and a3 != '-' and a4 != '-' and a5 != '-' and a6 != '-' and a7 != '-' and a8 != '-' and a9 != '-':
+    if a1 != '-' and a2 != '-' and a3 != '-' and a4 != '-' and a5 != '-' and a6 != '-' and a7 != '-' and a8 != '-' \
+            and a9 != '-':
         remiza = True
         return print('Egalitate'), remiza
 
+
 def verifica_o():
-    global a1, a2, a3, a4, a5, a6, a7, a8, a9, command_2
+    global a1, a2, a3, a4, a5, a6, a7, a8, a9, var_command_2
     validare_reincercare = False
-    if command_2 == 1 and a1 == '-':
+    if var_command_2 == 1 and a1 == '-':
         a1 = O
-    elif command_2 == 2 and a2 == '-':
+    elif var_command_2 == 2 and a2 == '-':
         a2 = O
-    elif command_2 == 3 and a3 == '-':
+    elif var_command_2 == 3 and a3 == '-':
         a3 = O
-    elif command_2 == 4 and a4 == '-':
+    elif var_command_2 == 4 and a4 == '-':
         a4 = O
-    elif command_2 == 5 and a5 == '-':
+    elif var_command_2 == 5 and a5 == '-':
         a5 = O
-    elif command_2 == 6 and a6 == '-':
+    elif var_command_2 == 6 and a6 == '-':
         a6 = O
-    elif command_2 == 7 and a7 == '-':
+    elif var_command_2 == 7 and a7 == '-':
         a7 = O
-    elif command_2 == 8 and a8 == '-':
+    elif var_command_2 == 8 and a8 == '-':
         a8 = O
-    elif command_2 == 9 and a9 == '-':
+    elif var_command_2 == 9 and a9 == '-':
         a9 = O
-    elif command_2 == 1 and a1 != '-' or command_2 == 2 and a2 != '-' or command_2 == 3 and a3 != '-' or command_2 == 4 and a4 != '-' or command_2 == 5 and a5 != '-' or command_2 == 6 and a6 != '-' or command_2 == 8 and a8 != '-' or command_2 == 7 and a7 != '-' or command_2 == 9 and a9 != '-':
+    elif var_command_2 == 1 and a1 != '-' or var_command_2 == 2 and a2 != '-' or var_command_2 == 3 and a3 != '-' \
+            or var_command_2 == 4 and a4 != '-' or var_command_2 == 5 and a5 != '-' or var_command_2 == 6 \
+            and a6 != '-' or var_command_2 == 8 and a8 != '-' or var_command_2 == 7 and a7 != '-' \
+            or var_command_2 == 9 and a9 != '-':
         while not validare_reincercare:
-            command_2 = int(input("Nu poti sa pui acolo, este deja completat"))
+            var_command_2 = int(input("Nu poti sa pui acolo, este deja completat"))
             verifica_o()
             validare_reincercare = True
-    elif command_2 > 9:
+    elif var_command_2 > 9:
         validare_reincercare = False
         while not validare_reincercare:
-            command_2 = int(input("Selecteaza un numar de la 1 la 9 numai: "))
+            var_command_2 = int(input("Selecteaza un numar de la 1 la 9 numai: "))
             verifica_o()
             validare_reincercare = True
 
@@ -92,35 +100,38 @@ def verifica_o():
 
 
 def verifica_x():
-    global a1, a2, a3, a4, a5, a6, a7, a8, a9, command
+    global a1, a2, a3, a4, a5, a6, a7, a8, a9, var_command
     validare_reincercare = False
-    if command == 1 and a1 == '-':
+    if var_command == 1 and a1 == '-':
         a1 = X
-    elif command == 2 and a2 == '-':
+    elif var_command == 2 and a2 == '-':
         a2 = X
-    elif command == 3 and a3 == '-':
+    elif var_command == 3 and a3 == '-':
         a3 = X
-    elif command == 4 and a4 == '-':
+    elif var_command == 4 and a4 == '-':
         a4 = X
-    elif command == 5 and a5 == '-':
+    elif var_command == 5 and a5 == '-':
         a5 = X
-    elif command == 6 and a6 == '-':
+    elif var_command == 6 and a6 == '-':
         a6 = X
-    elif command == 7 and a7 == '-':
+    elif var_command == 7 and a7 == '-':
         a7 = X
-    elif command == 8 and a8 == '-':
+    elif var_command == 8 and a8 == '-':
         a8 = X
-    elif command == 9 and a9 == '-':
+    elif var_command == 9 and a9 == '-':
         a9 = X
-    elif command == 1 and a1 != '-' or command == 2 and a2 != '-' or command == 3 and a3 != '-' or command == 4 and a4 != '-' or command == 5 and a5 != '-' or command == 6 and a6 != '-' or command == 8 and a8 != '-' or command == 7 and a7 != '-' or command == 9 and a9 != '-':
+    elif var_command == 1 and a1 != '-' or var_command == 2 and a2 != '-' or var_command == 3 and a3 != '-' \
+            or var_command == 4 and a4 != '-' or var_command == 5 and a5 != '-' or var_command == 6 and a6 != '-' \
+            or var_command == 8 and a8 != '-' \
+            or var_command == 7 and a7 != '-' or var_command == 9 and a9 != '-':
         while not validare_reincercare:
-            command = int(input("Nu poti sa pui acolo"))
+            var_command = int(input("Nu poti sa pui acolo"))
             verifica_x()
             validare_reincercare = True
-    elif command > 9:
+    elif var_command > 9:
         validare_reincercare = False
         while not validare_reincercare:
-            command = int(input("Selecteaza un numar de la 1 la 9:"))
+            var_command = int(input("Selecteaza un numar de la 1 la 9:"))
             verifica_x()
             validare_reincercare = True
     return a1, a2, a3, a4, a5, a6, a7, a8, a9
@@ -128,60 +139,62 @@ def verifica_x():
 
 # Tura BOTULUI
 def tura_npc():
-    global a1, a2, a3, a4, a5, a6, a7, a8, a9, command
+    global a1, a2, a3, a4, a5, a6, a7, a8, a9, var_command
     global npc_alege
     npc_alege = False
-    alegeri_npc = (3, 5 ,7 ,9, 1, 2, 4, 6, 8)
+    alegeri_npc = (3, 5, 7, 9, 1, 2, 4, 6, 8)
     while not npc_alege:
-        command = None
-        # alege prima cifra din alegeri_npc care e valabila in loc sa faca random si o verifica daca e goala si o pune in locul potrivit bazat pe regulile date
+        var_command = None
+        # alege prima cifra din alegeri_npc care e valabila in loc sa faca random si o verifica daca e goala
+        # si o pune in locul potrivit bazat pe regulile date
         for picks in alegeri_npc:
             if picks == 3 and a3 == '-':
-                command = 3
+                var_command = 3
                 a3 = X
                 npc_alege = True
                 break
             elif picks == 5 and a5 == '-':
-                command = 5
+                var_command = 5
                 a5 = X
                 npc_alege = True
                 break
             elif picks == 7 and a7 == '-':
-                command = 7
+                var_command = 7
                 a7 = X
                 npc_alege = True
                 break
             elif picks == 9 and a9 == '-':
-                command = 9
+                var_command = 9
                 a9 = X
                 npc_alege = True
                 break
             elif picks == 1 and a1 == '-':
-                command = 1
+                var_command = 1
                 a1 = X
                 npc_alege = True
                 break
             elif picks == 2 and a2 == '-':
-                command = 2
+                var_command = 2
                 a2 = X
                 npc_alege = True
                 break
             elif picks == 4 and a4 == '-':
-                command = 4
+                var_command = 4
                 a4 = X
                 npc_alege = True
                 break
             elif picks == 6 and a6 == '-':
-                command = 6
+                var_command = 6
                 a6 = X
                 npc_alege = True
                 break
             elif picks == 8 and a8 == '-':
-                command = 8
+                var_command = 8
                 a8 = X
                 npc_alege = True
                 break
-    return a1, a1, a3, a4, a5, a6, a7, a8, a9, print('Eu ALEG', command)
+    return a1, a1, a3, a4, a5, a6, a7, a8, a9, print('Eu ALEG', var_command)
+
 
 # un mic loop
 while True:
@@ -202,24 +215,24 @@ while True:
     castiga = False
     remiza = False
 
-# input cu niste conditii si mesaj de le NPC
+    # input cu niste conditii si mesaj de le NPC
     input_1 = input('>X si 0> (scrie incepe sau retry) ').lower()
     if input_1 == 'incepe' or input_1 == 'retry':
         ai = input('Vrei sa joci impotriva MEA :D ?: ')
         joc_inceput = True
         print('Eu sunt un NPC si o sa incerc sa te rup.')
-        nume_jucator = input('Cum te Cheama? ')
-        print("Hai sa vedem care pe care.")
+        nume_jucator = input('Cum te cheama bo$$ule? ')
+        print("Hai sa vedem care pe care. \n Mai jos o sa vezi tabla de joc si numerele unde poti sa pui tu 0")
         print(' ', 1, '|', 2, '|', 3, '\n ---------- \n', '', 4, '|', 5, '|', 6, '\n ----------\n', '', 7,
-                  '|', 8, '|', 9, '\n')
+              '|', 8, '|', 9, '\n')
 
-        while ai == 'da' or ai == 'yes' or ai == 'sigur' or ai == 'go' or ai == 'bot':
-            print('Yo eu sunt NPC.')
+        while ai == 'da' or ai == 'yes' or ai == 'sigur' or ai == 'da boss' or ai == 'bot':
+            print('Yo aleg primul.')
             nume_jucator1 = 'NPC'
             print("Hai sa incepem", nume_jucator.upper(), '\n\n')
             print(' ', 1, '|', 2, '|', 3, '\n ---------- \n', '', 4, '|', 5, '|', 6, '\n ----------\n', '', 7,
                   '|', 8, '|', 9, '\n')
-# isi face tura aici el primul cu X
+            # isi face tura aici el primul cu X
             print("NPC alege Acum")
             tura_npc()
             print('\n ', a1, '|', a2, '|', a3, '\n ---------- \n', '', a4, '|', a5, '|', a6, '\n ----------\n', '',
@@ -227,8 +240,8 @@ while True:
             conditii_castig()
             if castiga or remiza:
                 break
-# dupa aia pui tu 0 dupa el
-            command_2 = int(input('>> Unde vrei sa pui 0? (doar cifre intre 1-9): '))
+            # dupa aia pui tu 0 dupa el
+            var_command_2 = int(input(f'>> Unde vrei sa pui 0 {nume_jucator.upper()}? (doar cifre intre 1-9): '))
             verifica_o()
             print('\n ', a1, '|', a2, '|', a3, '\n ---------- \n', '', a4, '|', a5, '|', a6, '\n ----------\n', '',
                   a7, '|', a8, '|', a9, '\n')
--- Json_curs06/args_kwargs.py
+++ Json_curs06/args_kwargs.py
@@ -4,11 +4,25 @@
 #
 # print(my_function('string', 'string1', 'string2'))
 #
-def numbers_sum(*var):
-    suma = 0
-    print(var)
-    for item in var:
-        suma = suma + item
-    return suma
+# def numbers_sum(*var):
+#     suma = 0
+#     print(var)
+#     for item in var:
+#         suma = suma + item
+#     return suma
+#
+# print(numbers_sum(1,2, 3, 4, 69))
 
-print(numbers_sum(1,2, 3, 4, 69))
+
+# def diff_vars(a, *params):
+#     differenta = a
+#     for item in params:
+#         differenta = differenta - item
+#     return differenta
+#
+# print(diff_vars(1,2,3,4))
+
+def catalog(*args, **kwargs):
+    return args, kwargs.keys()
+
+print(catalog(1, 2, nume='Ionci', prenume='ViteaZUL', varsta='12'))
