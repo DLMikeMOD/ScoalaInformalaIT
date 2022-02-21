2b2fa8916963dd17f48957038e7b25c5b886e6a4
DLMikeMOD <fake_mail_mike@yahoo.com>
2022-02-21 19:57:18 +0200

    Teme updates

--- Teme/CNP validator.py
+++ Teme/CNP validator.py
@@ -1,10 +1,10 @@
 if __name__ == '__main__':
     cnp = input("Introdu CNP-ul: ")
-
+# oare este formatul de cnp corect, cifre sa fie exact 13 and so on
     if len(cnp) != 13:
         if cnp.isdigit() is False:
-            print('contains not digits')
-        print('invalid')
+            print('Unde is numerele?')
+        print('Format CNP invalid')
     else:
         sex = cnp[0]
         an = cnp[1:3]
@@ -13,26 +13,26 @@ if __name__ == '__main__':
         jj = cnp[7:9]
         nnn = cnp[9:12]
         c = cnp[-1]
-
+# validatus bucati din validator cu cifrele din tabel
         sex_ok = '1' <= sex <= '9'
         an_ok = '00' <= an <= '99'
         luna_ok = '01' <= luna <= '12'
         zi_ok = '01' <= zi <= '31'
         jj_ok = '01' <= jj <= '52'
         nnn_ok = '001' <= nnn <= '999'
-
+# incepe cifra de control de la 0 si definesc formual de control
         c_control = 0
         nr_control = '279146358279'
         for index, i in enumerate(cnp[:12]):
             c_control += int(i) * int(nr_control[index])
-
+# sa fie cu rest de 11 imparteala de control
         c_control = c_control % 11
-
+# some very complicated siens and math
         if c_control == 10:
             c_control = 1
-
+# well if it is equal to then it must be equal to
         c_ok = c_control == int(c)
-
+#show pleb his result
         if sex_ok and an_ok and luna_ok and zi_ok and jj_ok and nnn_ok and c_ok:
             print("CNP Valid, esti safe")
         else:
