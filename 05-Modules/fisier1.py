2bef5f4aed8fc939dcc030e75e7323515bd2253c
DLMikeMOD <fake_mail_mike@yahoo.com>
2022-05-04 22:43:00 +0300

    Delete 05-Modules directory

--- 05-Modules/fisier1.py
+++
@@ -1,8 +0,0 @@
-# import modulus  #asa se importa tot
-from modulus import my_sum as suma #asa se importa doar ceva anume din alt modul also poti sa redenumesti functia imporata cu "as" and add new name
-# from Function04.calculator import suma
-# import math
-# from math import fsum
-if __name__ == '__main__':
-    print(suma(6,9), __name__)
-
