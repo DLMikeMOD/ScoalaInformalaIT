b35d6de9af61e20f186746a47b6413412a5f1eaf
DLMikeMOD <fake_mail_mike@yahoo.com>
2022-05-05 13:03:19 +0300

    Selenium homework CSV Covid

--- Teme/Selenium work/borderline-ddos.py
+++ Teme/Selenium work/borderline-ddos.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 # import pandas as pd
+#not needed anymore
 import matplotlib.pyplot as plt
 
 # og url in F String format
